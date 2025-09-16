2fe9aeb357c79a52e8e40a18d67369fa621fca1f
Zanejay <cjq@gmx.de>
2025-09-16 11:09:15 +0200

    Task1

--- machine.py
+++ machine.py
@@ -1,6 +1,28 @@
-menu = {
-  "Latte": 5.0,
-  "Espresso":  3.0,
-  "Cappuccino":  4.5,
-}
-print("Eersefs")
+Latte = 5.0
+Espresso =  3.0
+Cappuccino =  4.5
+print("Latte",Latte,"€",  "\nEspresso",Espresso,"€", "\nCappuccino",Cappuccino,"€")
+
+while True:
+  
+  print("Bitte ein Getränk auswählen oder für abbruch 'ausschalten' eingeben\n  ")
+
+  eingabe = input()
+
+  if eingabe == "Latte":
+    print("Hier ist der Latte")
+    continue
+  elif eingabe == "Espresso":
+    print("Hier ist der Espresso")
+    continue
+  elif eingabe == "Cappuccino":
+    print("Hier ist der Cappuccino")
+    continue
+  elif eingabe == "ausschalten":
+    print("Kaffeemaschiene wird ausgeschaltet")
+    break
+  else:
+    print("Fehlerhafte Eingabe, bitte erneut probieren")
+    continue
+
+  
