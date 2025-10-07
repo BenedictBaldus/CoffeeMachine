0c5aaeaf0045533845f89826b0c35294fd6df2a4
BenedictBaldus <benedict@benedictbaldus.de>
2025-10-07 09:45:49 +0200

    Dictonary für Coffe erstellt.

--- machine.py
+++ machine.py
@@ -1,14 +1,33 @@
 Latte = 5.0
 Espresso =  3.0
 Cappuccino =  4.5
-print("Latte",Latte,"€",  "\nEspresso",Espresso,"€", "\nCappuccino",Cappuccino,"€")
+#print("Latte",Latte,"€",  "\nEspresso",Espresso,"€", "\nCappuccino",Cappuccino,"€")
+
+coffe = { "Latte" : 5.0,
+          "Espresso" : 3.0,
+          "Cappuccino" : 4.5 }
+
+for drink,preis in coffe.items():
+  print(drink, "kostet", preis,"€")
+
+
+summe = 0
+
 
 while True:
-  
-  print("Bitte ein Getränk auswählen oder für das Ausschalten der Maschine 'off' eingeben\n  ")
+  eingabe_coins = []
+  print("\nBitte ein Getränk auswählen oder für das Ausschalten der Maschine 'off' eingeben\n  ")
   eingabe = input()
 
   if eingabe == "Latte" or eingabe == "latte":
+    print(coffe.get("Latte"))
+    print("Werfen Sie bitte das Geld ein.")
+    
+    while sum(eingabe_coins)<coffe.get("Latte"):
+      eingabe_coins.append(float(input()))
+      print(sum(eingabe_coins))
+
+
     print("Hier ist der Latte")
     continue
   elif eingabe == "Espresso" or eingabe == "espresso":
