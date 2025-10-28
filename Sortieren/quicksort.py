2049fb96aea0133a57579388c2c7ef8ea29feea4
BenedictBaldus <benedict@benedictbaldus.de>
2025-10-28 12:10:26 +0100

    Quicksort

--- Sortieren/quicksort.py
+++ Sortieren/quicksort.py
@@ -3,23 +3,30 @@ import random
 list=[]
 for i in range (0,10):
     list.append(random.randint(1,100))
-print(list)
+print("Unsortiert", list)
 
 def quicksort(arr,start,ende):
-    pivot =arr[0]
-    i= start
-    j= ende-1
-    if i == j:
-        
-    while i<=j:
-        if arr[i]>=arr[j]:
-            arr[i],arr[j]=arr[j],arr[i]
-        i +=1
-        j -=1
+    if start >= ende:
+        return
+    pivot =arr[start]
+    i= start+1
+    j= ende
     
-    print(arr)
-    quicksort(arr,0,i)
-    quicksort(arr,i+1,len(arr))
+    while i <= j:     
+        while i <= j and arr[i] <= pivot:
+            i += 1
+        while i <= j and arr[j] > pivot:
+            j -= 1
+        if i < j:
+            arr[i], arr[j] = arr[j], arr[i]
+            print(arr) 
+    
+    arr[start], arr[j] = arr[j], arr[start]
 
-quicksort(list,0,len(list))
+    quicksort(arr, start, j - 1)
+    quicksort(arr, j + 1, ende)
+
+
+
+quicksort(list,0,len(list)-1)
 
