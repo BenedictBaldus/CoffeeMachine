15ed72f09504d6aa47f3b79ea6a5fa77422fefc4
BenedictBaldus <benedict@benedictbaldus.de>
2025-11-18 14:29:19 +0100

    task6

--- machine.py
+++ machine.py
@@ -1,3 +1,4 @@
+import datetime
 coffe = { "Latte Macchiato" : {                 #Preis in €, Wasser in ml, Coffe in mg, Oatmilk in ml
             "Preis" : 5.0,
             "Water" : 100,
@@ -18,84 +19,128 @@ coffe = { "Latte Macchiato" : {                 #Preis in €, Wasser in ml, Cof
           } }
 #Ressources
 water = 2000 # in ml
-grinder =500 # in g
+grinder = 500 # in g
 oatmilk = 1000 # in ml
-
+einnahmen = 0
 cashdrawer= {
 "10 Cent" : 10,
 "20 Cent" : 10,
 "50 Cent" : 10,
-"1 Euro" : 10,
+"1 Euro" : 0,
 "2 Euro" : 10,
 }
 
 akzeptiere_muenzen = [0.1, 0.2, 0.5, 1.0, 2.0]
   
 
-def ausgabe(drink):
+def einwurf_muenzen(drink):
   print("Werfen Sie bitte das Geld ein.")
   price = coffe[drink]["Preis"]
-
+  global einnahmen
   while sum(eingabe_coins)<price:
       while True:
-        temp_eingabe = float(input())
-        if temp_eingabe == 2.0:
-          cashdrawer.update({"2 Euro": cashdrawer["2 Euro"]+1})
-        elif temp_eingabe == 1.0:
-          cashdrawer.update({"1 Euro": cashdrawer["1 Euro"]+1})
-        elif temp_eingabe == 0.5:
-          cashdrawer.update({"50 Cent": cashdrawer["50 Cent"]+1})
-        elif temp_eingabe == 0.2:
-          cashdrawer.update({"20 Cent": cashdrawer["20 Cent"]+1})
-        elif temp_eingabe == 0.1:
-          cashdrawer.update({"10 Cent": cashdrawer["10 Cent"]+1})
-      
+        temp_eingabe = input()
 
-        
-
-        if temp_eingabe in akzeptiere_muenzen:                                                                      #Prüfen ob die eingegebenen Münzen akzeptiert werden
-          eingabe_coins.append(temp_eingabe)                                                                            
-          print(f"Es wurden {round(sum(eingabe_coins),2)}€ eingeworfen.")
-
-          if(sum(eingabe_coins)<price):                                                                                 #Prüfen ob die eingeworfenen Münzen zu wenig sind
-            print("Das eingeworfene Geld reicht nicht aus, bitte mehr Geld einwerfen")
-          elif(sum(eingabe_coins)>price):                                                                               #Prüfen ob die eingeworfenen Münzen zu viel sind
-            rueckgeld(rueckgeld,price)
-            print("He",eingabe_coins)
-            break
-          else:
-            print("Ho",eingabe_coins)
-            break 
-
+        if(temp_eingabe.lower() == "abbruch"):
+          print("Der Vorgang wird abgebrochen")
+          rueckgeld(eingabe_coins,0)
+          return
         else:
-          print("Ungültige Münze, es werden nur 0.1,0.2,0.5,1 oder 2 Euro Münzen akzeptiert.")                          #Ausgabe das ungültige Münzen eingeworfen wurden
-          continue
+          temp_eingabe = float(temp_eingabe)
+
+          if temp_eingabe == 2.0:
+            cashdrawer.update({"2 Euro": cashdrawer["2 Euro"]+1})
+            einnahmen += 2
+          elif temp_eingabe == 1.0:
+            cashdrawer.update({"1 Euro": cashdrawer["1 Euro"]+1})
+            einnahmen += 1
+          elif temp_eingabe == 0.5:
+            einnahmen += 0.5
+            cashdrawer.update({"50 Cent": cashdrawer["50 Cent"]+1})
+          elif temp_eingabe == 0.2:
+            cashdrawer.update({"20 Cent": cashdrawer["20 Cent"]+1})
+            einnahmen += 0.2
+          elif temp_eingabe == 0.1:
+            cashdrawer.update({"10 Cent": cashdrawer["10 Cent"]+1})
+            einnahmen += 0.1
+
+          if temp_eingabe in akzeptiere_muenzen:                                                                      #Prüfen ob die eingegebenen Münzen akzeptiert werden
+            eingabe_coins.append(temp_eingabe)                                                                            
+            print(f"Es wurden {round(sum(eingabe_coins),2)}€ eingeworfen.")
+
+            if(sum(eingabe_coins)<price):                                                                                 #Prüfen ob die eingeworfenen Münzen zu wenig sind
+              print("Das eingeworfene Geld reicht nicht aus, bitte mehr Geld einwerfen. Oder für Abbrechen 'Abruch' eingeben")
+            elif(sum(eingabe_coins)>price):                                                                               #Prüfen ob die eingeworfenen Münzen zu viel sind
+              rueckgeld(rueckgeld,price)
+              ressourcen(drink)
+              break
+            else:
+              ressourcen(drink)
+              break
+          else:
+            print("Ungültige Münze, es werden nur 0.1,0.2,0.5,1 oder 2 Euro Münzen akzeptiert.")                          #Ausgabe das ungültige Münzen eingeworfen wurden
+            continue
 
 def report():
-  print("Wasser", water)
-  print("Milch", oatmilk)
-  print("Kaffe", grinder)
-  print(cashdrawer)
+  global einnahmen
+  print(f"Wasser {water}")
+  print(f"Milch {oatmilk}")
+  print(f"Kaffe {grinder}")
+  
+  print(f"Einnahmen {einnahmen}\n")
+
+  report_name = "report.txt"
+  with open(report_name, "w") as datObj:
+  #datObj = open(report_name, "w")
+    datObj.write(f"Wasser: {water}\n")
+    datObj.write(f"Milch: {oatmilk}\n")
+    datObj.write(f"Kaffe: {grinder}\n")
+    datObj.write(f"Einnahmen: {einnahmen}\n")
+  
+
+def replenish():
+  water = 2000 # in ml
+  grinder = 500 # in g
+  oatmilk = 1000 # in ml
+  timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+  with open("log.txt", "a") as datObj:
+    datObj.write(f"{timestamp} Replenished\r\n")
+
 
 def rueckgeld(rueckgeld,price):
+  global einnahmen
   rueckgeld=round(sum(eingabe_coins)-price, 2)
   print(f"Sie bekommen {rueckgeld}€ Rückgeld")
   while rueckgeld>0:
-    if rueckgeld>= 2.0:
+    if rueckgeld>= 2.0 and cashdrawer["2 Euro"]>0:
       rueckgeld = round(rueckgeld -2.0, 2)
+      cashdrawer.update({"2 Euro": cashdrawer["2 Euro"]-1})
       print("Es werden 2.0€ ausgegeben")
-    elif rueckgeld>=1.0:
+      einnahmen -=2
+    elif rueckgeld>=1.0 and cashdrawer["1 Euro"]>0:
       rueckgeld = round(rueckgeld -1.0, 2)
+      cashdrawer.update({"1 Euro": cashdrawer["1 Euro"]-1})
+      einnahmen -=1
       print("Es werden 1.0€ ausgegeben")
-    elif rueckgeld>=0.5:
+    elif rueckgeld>=0.5 and cashdrawer["50 Cent"]>0:
       rueckgeld = round(rueckgeld -0.5, 2)
+      cashdrawer.update({"50 Cent": cashdrawer["50 Cent"]-1})
+      einnahmen -=0.5
       print("Es werden 0.5€ ausgegeben")
-    elif rueckgeld>=0.2:
+    elif rueckgeld>=0.2 and cashdrawer["20 Cent"]>0:
       rueckgeld = round(rueckgeld -0.2, 2)
+      cashdrawer.update({"20 Cent": cashdrawer["20 Cent"]-1})
+      einnahmen -=0.2
       print("Es werden 0.2€ ausgegeben")
-    elif rueckgeld>=0.1:
+    elif rueckgeld>=0.1 and cashdrawer["10 Cent"]>0:
       rueckgeld = round(rueckgeld -0.1, 2)
+      cashdrawer.update({"10 Cent": cashdrawer["10 Cent"]-1})
+      einnahmen -=0.1
       print("Es werden 0.1€ ausgegeben")
+    else:
+      print("Nicht genügend Rückgeld vorhanden")
+      break
+
 def ressourcen(drink):
   global water
   global oatmilk
@@ -104,10 +149,10 @@ def ressourcen(drink):
     water = water - coffe[drink]["Water"]
     oatmilk = oatmilk - coffe[drink]["Oatmilk"]
     grinder = grinder - coffe[drink]["Coffe"]
-    print(f"Der {drink} wird ausgegeben\n ")
+    print(f"\nDer {drink} wird ausgegeben\n ")
     
   else:
-    print("Bitte Ressourcen nachfüllen")
+    print("\nBitte Ressourcen nachfüllen\n")
 
 while True:
   eingabe_coins = []
@@ -115,33 +160,35 @@ while True:
   for drink,preis in coffe.items():
     print(f"{count}: {drink} kostet {preis["Preis"]}€")
     count+=1
-  print("\nBitte ein Getränk auswählen oder für das Ausschalten der Maschine 'off' eingeben\n  ")
+  print("\nBitte ein Getränk auswählen oder für das Ausschalten der Maschine 'off'. Für den Report 'report' oder für das Nachfüllen 'replenish' ein. \n  ")
   eingabe = input()
   if eingabe.lower() == "off":
     print("Kaffeemaschiene wird ausgeschaltet")
     break
+  elif eingabe.lower() == "report":
+    report()
+  elif eingabe.lower() == "replenish":
+    replenish()
 
   match eingabe:
     case "1":
       drink = "Latte Macchiato"
       print(f"Es wurde der {drink} gewählt")
-      ausgabe(drink)
-      ressourcen(drink)
+      einwurf_muenzen(drink)
       
     case "2":
       drink = "Espresso"
       print(f"Es wurde der {drink} gewählt")
-      ausgabe(drink)
-      ressourcen(drink)
+      einwurf_muenzen(drink)
+      
       
     case "3":
       drink = "Cappucino"
       print(f"Es wurde der {drink} gewählt")
-      ausgabe(drink)
-      ressourcen(drink)
+      einwurf_muenzen(drink)
       
     case _:
-      print("Fehlerhafte Eingabe, bitte erneut probieren")
+      print("Fehlerhafte Eingabe, bitte erneut probieren\n")
       continue
 
   
